1fe73b2cf2b97f4840361d81590e2b29be9f3bd1
tdamron <TristanDamron31@gmail.com>
2020-05-12 18:24:14 -0700

    Separated references, tutorials, discussions, and how-to sections into dedicated pages

--- convert.rb
+++ convert.rb
@@ -1,9 +1,10 @@
 # Safe script for converting Asciidoc file to HTML
+# Converts all .adoc files in the parent director to .html files
 
 require 'asciidoctor'
 require 'asciidoctor/cli/options'
 require 'asciidoctor/cli/invoker'
 
 require 'asciidoctor-diagram'
-
-Asciidoctor.convert_file 'README.adoc', to_file: true, safe: :safe
+ 
+Dir["./*.adoc"].each { |x| Asciidoctor.convert_file x, to_file: true, safe: :safe }
