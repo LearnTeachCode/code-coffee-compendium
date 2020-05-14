d9934fa139bc23818731a9ab0d70b6179fec313a
tdamron <TristanDamron31@gmail.com>
2020-05-14 11:19:14 -0700

    Auto generate a sitemap puml file on document conversion to HTML

--- convert.rb
+++ convert.rb
@@ -1,3 +1,4 @@
+
 # Safe script for converting Asciidoc file to HTML
 # Converts all .adoc files in the parent director to .html files
 
@@ -6,5 +7,45 @@ require 'asciidoctor/cli/options'
 require 'asciidoctor/cli/invoker'
 
 require 'asciidoctor-diagram'
- 
-Dir["./*.adoc"].each { |x| Asciidoctor.convert_file x, to_file: true, safe: :safe }
+
+sitemappuml = File.open("sitemap.puml", "w")
+
+sitemappuml.write "@startuml\n"
+sitemappuml.write "hide empty description\n"
+
+Dir["./*.adoc"].each { |x|
+  f = File.open(x, "r")
+  
+  filename = x.gsub(/.\//, "")
+  filename = filename.gsub(/.adoc/, "")
+  filename = filename.gsub(/-/, "")   
+
+  f.each_line do |line|
+    if line.start_with? "="
+      if not line.include? "image"
+        level = line.count("=")
+        if level <= 2
+          line = line.gsub(/\s+/, "")
+          line = line.gsub(/=+/, "")
+          line = line.gsub(/<</, "")
+          line = line.gsub(/>>/, "")
+          line = line.gsub(/\)/, "")       
+          line = line.gsub(/\(/, "")
+          line = line.gsub(/-/, "")
+          line = line.gsub(/#/, "")
+          line = line.gsub(/\,/, "")                 
+          sitemappuml.write filename + " -down-> " + line + "\n"
+        end      
+      end      
+    end    
+  end  
+
+  f.close  
+
+  Asciidoctor.convert_file x, to_file: true, safe: :safe  
+}
+
+sitemappuml.write "@enduml"
+
+sitemappuml.close
+puts "Sitemap generated successfully."
