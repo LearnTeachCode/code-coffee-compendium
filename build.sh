0f8388802e5054c831e45e36dd856d1a80838e49
Sean Marquez <capsulecorplab@gmail.com>
2020-12-04 07:55:00 -0800

    add patch for redirecting site to home/

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [ ! -r ./public ]; then
-    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.76.5-alpine 'hugo --minify -v --destination public'
+    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.76.5-alpine 'hugo --minify -v --destination public && cp index.html public/'
 fi
